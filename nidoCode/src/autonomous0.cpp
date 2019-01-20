58ccfea537c346170aad69a3d6f8f441fe1973e6
Pranav Srisankar <pranavsri623@gmail.com>
2019-01-20 12:25:11 -0800

    rihjt

---
+++ nidoCode/src/autonomous0.cpp
@@ -0,0 +1,234 @@
+/*#include "main.h"
+#include "init.h"
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+/*int rb = -1; // 1 for blue; -1 for red
+
+ void movebase(int l, int r) {
+  mtr3.move(l);
+  mtr4.move(l);
+  mtr1.move(r);
+  mtr2.move(r);
+ }
+
+ void moveticks(int l, int vl, int r, int vr) {
+  mtr3.move_relative(l, vl);
+  mtr4.move_relative(l, vl);
+  mtr1.move_relative(r, vr);
+  mtr2.move_relative(r, vr);
+ }
+
+ void Flag() {
+
+   pwrup.suspend();
+   pwrdwn.suspend();
+   pros::delay(25);
+
+   pwrup.resume();
+   pros::delay(2000);
+   //intake.move_velocity(-200);
+   intake.move(-120);
+   pros::delay(1000);
+   intake.move(0);
+
+
+
+   pros::delay(1);
+
+   moveticks(-950 * rb, -100 * rb, 950 * rb, 100 * rb);
+   pros::delay(1000);
+
+   moveticks(3000, 200, 3000, 200);
+   intake.move(-50);
+   pros::delay(2000);
+   intake.move_velocity(-200);
+   pros::delay(700);
+   intake.move_velocity(0);
+   moveticks(-400, -150, -400, -150);
+
+   pros::ADIDigitalIn limit_switch(2);
+   while(true){
+ 		if (limit_switch.get_value() == 0){
+ 			intake.move(-121);
+ 		}
+ 		else if (limit_switch.get_value() == 1) {
+      intake.move(200);
+      pros::delay(250);
+      intake.move(0);
+      //moveticks(-400, -150, -400, -150);
+ 			break;
+ 		}
+  }
+   pros::delay(1);
+
+   moveticks(-2750, -150, -2750, -150);
+   pros::delay(1500);
+
+   moveticks(1000 * rb, 100 * rb, -1000 * rb, -100 * rb);
+   pros::delay(1100);
+
+   moveticks(1600, 150, 1600, 150);
+   pros::delay(1500);
+
+   intake.move_velocity(-200);
+   pros::delay(700);
+   intake.move(0);
+
+   moveticks(3000, 200, 3000, 120);
+   pros::delay(1000);
+   movebase(0, 0);
+   intake.move(0);
+
+   pwrup.suspend();
+   pwrdwn.resume();
+   //skills
+   moveticks(-4200, -200, -4200, -200);
+   pros::delay(1000);
+   moveticks(-1000 * rb, -100 * rb, 1000 * rb, 100 * rb);
+   pros::delay(1000);
+   moveticks(4200, 170, 4200, 170);
+
+ }
+
+ void blueAlliance() {
+
+   pwrup.resume();
+   pros::delay(4000);
+   intake.move(-120);
+   pros::delay(2000);
+   pwrup.suspend();
+   pwrdwn.resume();
+   pros::delay(1000);
+   pwrdwn.suspend();
+   if(true) {
+     moveticks(-900, -100, 900, 100);
+     pros::delay(1500);
+     moveticks(3000, 100, 3000, 100);
+     pros::delay(3000);
+     moveticks(900, 100, -900, -100);
+     pros::delay(1500);
+     moveticks(-1500, -100, -1500, -100);
+     movebase(0, 0);
+   }
+ }
+ void backAuton(){
+
+   //move forward to be in line with platform
+   moveticks(1600, 150, 1600, 150);
+   pros::delay(1500);
+
+   //90 deg turn towards platform (currently red side)
+   moveticks(-1000 * rb, -100 * rb, 1000 * rb, 100 * rb);
+   pros::delay(1100);
+   //move towards platform
+   moveticks(2500, 200, 2500, 200);
+   pros::delay(3000);
+ }
+
+ void skills() {
+   pwrup.suspend();
+   pwrdwn.suspend();
+   pros::delay(25);
+
+   pwrup.resume();
+   pros::delay(4000);
+   intake.move_velocity(-200);
+   pros::delay(1000);
+   intake.move(0);
+   pros::delay(1);
+
+   moveticks(-880 * rb, -100 * rb, 880 * rb, 100 * rb);
+   pros::delay(1500);
+
+   moveticks(3000, 150, 3000, 150);
+   intake.move(-50);
+   pros::delay(2000);
+
+   intake.move_velocity(-170);
+   pros::delay(700);
+   intake.move_velocity(0);
+   pros::delay(1);
+
+   moveticks(-2900, -150, -2900, -150);
+   pros::delay(1600);
+
+   moveticks(900 * rb, 100 * rb, -900 * rb, -100 * rb);
+   pros::delay(1300);
+
+   moveticks(1600, 150, 1600, 150);
+   pros::delay(1500);
+
+   intake.move_velocity(-200);
+   pros::delay(1100);
+   intake.move(0);
+
+   moveticks(1000 * rb, 100 * rb, -1000 * rb, -100 * rb);
+   pros::delay(1300);
+
+   moveticks(500, 100, 500, 100);
+   pros::delay(700);
+
+   flipper.move(-100);
+   pros::delay(500);
+
+   moveticks(-800, -100, -800, -100);
+   pros::delay(800);
+
+   flipper.move(127);
+   pros::delay(1000);
+
+   moveticks(0, 0, 800, 100);
+   pros::delay(800);
+
+   moveticks(-800, -100, -800, -100);
+   pros::delay(800);
+
+   moveticks(900, 100, -900, -100);
+   pros::delay(1200);
+
+   moveticks(1500, 150, 1500, 150);
+   pros::delay(1500);
+
+   moveticks(900, 100, -900, -100);
+   pros::delay(1200);
+
+   moveticks(3000, 150, 3000, 150);
+   intake.move(127);
+   pros::delay(2000);
+
+   moveticks(-3000, -150, -3000, -150);
+   pros::delay(2000);
+
+   moveticks(800, 100, 800, 100);
+   pros::delay(800);
+
+   moveticks(-900, -100, 900, 100);
+   pros::delay(1200);
+
+   moveticks(1500, 150, 1500, 150);
+   pros::delay(1500);
+
+   moveticks(1000, 100, -1000, -100);
+   pros::delay(1300);
+
+   moveticks(3000, 150, 3000, 150);
+   intake.move(127);
+   pros::delay(2000);
+
+ }
+
+ void no_auton() {}
+
+void autonomous() {
+  rpm = 200;
+  Flag();
+}*/
